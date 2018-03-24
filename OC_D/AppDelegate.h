b88fab58f632e5eae8d122c4f3b9efe62f07b2ec
idea <idea@idea.local>
2018-03-24 09:12:26 +0800

    Initial Commit

---
+++ OC_D/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OC_D
+//
+//  Created by idea on 2018/3/24.
+//  Copyright © 2018年 idea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC_D/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OC_D
+//
+//  Created by idea on 2018/3/24.
+//  Copyright © 2018年 idea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
